7aefdd704f56c8b1bf39866b06508a367b9e568b
AndyZheng430 <zhenga6@bxscience.edu>
2019-12-07 00:36:52 -0500

    default files

--- src/App.js
+++ src/App.js
@@ -7,10 +7,7 @@ import { firebaseConnect } from 'react-redux-firebase';
 import Navbar from './components/navbar/Navbar.js';
 import RegisterScreen from './components/register_screen/RegisterScreen.js';
 import LoginScreen from './components/login_screen/LoginScreen.js';
-import HomeScreen from './components/home_screen/HomeScreen.js';
-import ListScreen from './components/list_screen/ListScreen.js';
 import DatabaseTester from './test/DatabaseTester';
-import ItemScreen from './components/item_screen/ItemScreen.js';
 
 class App extends Component {
 
@@ -29,9 +26,6 @@ class App extends Component {
               <Route path="/databaseTester" component={DatabaseTester} />
               <Route path="/register" component={RegisterScreen} />
               <Route path="/login" component={LoginScreen} />
-              <Route path="/todoList/:id/:key" component={ItemScreen} />
-              <Route path="/todoList/:id" component={ListScreen} />
-              <Route path="/:any" component={HomeScreen} />
             </Switch>
           </div>
         </BrowserRouter>
